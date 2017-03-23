65610d09ce29c8971d77dd41c79775b1a76264e8
nolancw98 <nolancw98@Lenovo-PC>
2017-03-23 09:52:29 -0500

    Began file reader and writer

---
+++ SMPlatform/src/smplatform/LoginReader.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package smplatform;
+import java.io.FileReader;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.BufferedReader;
+/**
+ *
+ * @author nolancw98
+ */
+public class LoginReader{
+    private String log;
+    private 
+    public LoginReader()
+    {
+        log = "users.txt";
+    }
+    
+    public void write()
+    {
+        
+    }
+    
+    public void read()
+    {
+        
+    }
+}
