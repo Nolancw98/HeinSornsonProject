9c8f27976757ac92fe10bf6aed0013ecb91dfaff
nolancw98 <nolancw98@Lenovo-PC>
2017-04-04 14:13:09 -0500

    Doing login stuff

--- SMPlatform/src/smplatform/ChatServer.java
+++ SMPlatform/src/smplatform/ChatServer.java
@@ -7,7 +7,9 @@ package smplatform;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.ObjectInputStream;
 import java.io.PrintWriter;
+import java.io.Reader;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.ArrayList;
@@ -25,6 +27,7 @@ public class ChatServer {
     //Unique names of clients
     private static HashSet<String> names = new HashSet<String>();
     private static HashSet<String> passwords = new HashSet<String>();
+    private static HashSet<Entry> logins = new HashSet<Entry>();
     
     //Set of print writers for all clients
     private static HashSet<PrintWriter> writers = new HashSet<PrintWriter>();
@@ -51,11 +54,14 @@ public class ChatServer {
             listener.close();
         }
     }
-    private static class Handler extends Thread {
+    private static class Handler extends Thread{
+        private Entry userpass;
         private String name;
+        private String pass;
         private int newUser;
         private Socket socket;
         private BufferedReader in;
+        private ObjectInputStream objin;
         private PrintWriter out;
         
         public Handler(Socket socket)
@@ -67,38 +73,50 @@ public class ChatServer {
         {
             try
             {
+                
                 in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                //objin = new ObjectInputStream(socket.getInputStream());  BREAKS STUFF
                 out = new PrintWriter(socket.getOutputStream(), true);
                 while(true)
                 {
                     out.println("NEWUSER");
                     newUser = in.read();
-                    if(newUser == 0)
+                    System.out.println(newUser);
+                    if(newUser == 48)
                     {
                         out.println("CREATEACCOUNT");
+                        try{
+                            userpass = (Entry) objin.readObject();
+                        }
+                        catch(Exception e)
+                        {
+                        }
+                        
+                        if(userpass == null)
+                        {
+                            return;
+                        }
+                        synchronized(logins)
+                        {
+                            //Check user pass combinations for conflicts
+                            if(!logins.contains(userpass)){
+                                logins.add(userpass);
+                                break;
+                            }
+                        }
+                    }
+                    else if(newUser == 49)
+                    {
+                        out.println("SUBMITNAME");
                         name = in.readLine();
                         if(name == null)
                         {
                             return;
                         }
-                        synchronized(names)
+                        if(names.contains(name))
                         {
-                            if(!names.contains(name)){
-                                names.add(name);
-                                break;
-                            }
+                            return;
                         }
-                        
-                    }
-                    out.println("SUBMITNAME");
-                    name = in.readLine();
-                    if(name == null)
-                    {
-                        return;
-                    }
-                    if(names.contains(name))
-                    {
-                        break;
                     }
                     
                 }
