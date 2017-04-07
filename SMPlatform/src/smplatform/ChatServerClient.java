872d674fe56eac2eaf55bc70a35833d30aced7ef
edwardhein <edwardhein@172.30.17.79>
2017-04-06 20:08:04 -0500

    040617 EDDIE CHANGES

--- SMPlatform/src/smplatform/ChatServerClient.java
+++ SMPlatform/src/smplatform/ChatServerClient.java
@@ -44,11 +44,14 @@ public class ChatServerClient {
         });
     }
     
-    private Entry getNewUser()
+    private String getNewUser()
     {
-        String user = JOptionPane.showInputDialog(frame, "Enter your desired Username", "Register", JOptionPane.QUESTION_MESSAGE);
-        String pass = JOptionPane.showInputDialog(frame, "Enter your Password", "Register", JOptionPane.QUESTION_MESSAGE);
-        return new Entry(user, pass);
+        return JOptionPane.showInputDialog(frame, "Enter your desired Username", "Register", JOptionPane.QUESTION_MESSAGE);
+    }
+    
+    private String getNewPass()
+    {
+        return JOptionPane.showInputDialog(frame, "Enter your Password", "Register", JOptionPane.QUESTION_MESSAGE);
     }
     
     private int getReturning()
@@ -82,6 +85,10 @@ public class ChatServerClient {
             {
                 out.println(getNewUser());
             }
+            else if(line.startsWith("CREATEPASS"))
+            {
+                out.println(getNewPass());
+            }
             else if(line.startsWith("SUBMITNAME"))
             {
                 out.println(getName());
@@ -92,9 +99,8 @@ public class ChatServerClient {
             }
             else if(line.startsWith("MESSAGE"))
             {
-                messageArea.append(line.substring(8) + "\n");
+                messageArea.append(line.substring(0) + "\n");
             }
-      
         }
     }
 
